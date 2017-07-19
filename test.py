90a9a7781c0a279db781064644186911f677989d
drklee3 <drklee3@users.noreply.github.com>
2017-07-19 14:48:22 -0700

    clean up & create uploads folder if not exist

--- test.py
+++ test.py
@@ -9,59 +9,22 @@ import convert
 UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/test')
-def test():
-	return url_for('show_user_profile',username = 'Frank', age = 12)
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         f = request.files['file']
-#         mp3name = secure_filename(f.filename)
-#         f.save(os.path.join(UPLOAD_FOLDER, mp3name))
-#         wavname = mp3name[:len(mp3name) - 4] + ".wav"
-#         convert.convert(mp3name, wavname)
-
-#         classification = classify.runClassify(wavname)
-#         os.remove(os.path.join(UPLOAD_FOLDER, wavname))
-#     return classification
-
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        mp3name = secure_filename(f.filename)
-        f.save(os.path.join(UPLOAD_FOLDER, mp3name))
-        wavname = mp3name[:len(mp3name) - 4] + ".wav"
-        convert.convert(mp3name, wavname)
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
 
-        classification = classify.runClassify(wavname)
-        os.remove(os.path.join(UPLOAD_FOLDER, wavname))
+    f = request.files['file']
+    mp3name = secure_filename(f.filename)
+    f.save(os.path.join(UPLOAD_FOLDER, mp3name))
+    wavname = mp3name[:len(mp3name) - 4] + ".wav"
+    convert.convert(mp3name, wavname)
 
-        print(request.form['type'])
-        print(request.form['price'])
-        #TODO: Choose Headphones based off of these params
+    classification = classify.runClassify(wavname)
+    os.remove(os.path.join(UPLOAD_FOLDER, wavname))
+
+    print(request.form['type'])
+    print(request.form['price'])
+    #TODO: Choose Headphones based off of these params
 
     return classification
-
-
